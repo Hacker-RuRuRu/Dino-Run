45a829f651a1c7191c0b7fb0655fbf72089a0057
Hacker-RuRuRu <wiebrecht.timo@gmail.com>
2026-06-16 22:38:13 +0200

    Final 16.0626 22:38

--- Dino_Run.py
+++ Dino_Run.py
@@ -1,6 +1,80 @@
 import pygame
 from sys import exit
-from random import randint
+from random import randint, choice
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk1 = pygame.image.load('Dino-Run/graphics/Player/player_walk_1.png').convert_alpha()
+        player_walk2 = pygame.image.load('Dino-Run/graphics/Player/player_walk_2.png').convert_alpha()
+        self.player_walk = [player_walk1,player_walk2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('Dino-Run/graphics/Player/jump.png').convert_alpha()
+        
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom = (200,300))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('Dino-Run/audio/jump.mp3')
+        self.jump_sound.set_volume(0.1)
+
+    def jump(self):
+        if self.rect.bottom >= 300:    
+            self.gravity = -20
+            self.jump_sound.play()
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+    def reset_position(self):
+        self.rect.bottom = 300
+        self.gravity = 0
+
+    def update(self):
+        self.apply_gravity()   
+        self.animation_state()   
+    
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self,type):
+        super().__init__()
+        if type == 'fly':
+            fly_1 = pygame.image.load('Dino-Run/graphics/Fly/Fly1.png').convert_alpha()
+            fly_2 = pygame.image.load('Dino-Run/graphics/Fly/Fly2.png').convert_alpha()
+            self.frames = [fly_1,fly_2]
+            y_pos = 210
+        else:
+            snail_1 = pygame.image.load('Dino-Run/graphics/snail/snail1.png').convert_alpha()
+            snail_2 = pygame.image.load('Dino-Run/graphics/snail/snail2.png').convert_alpha()
+            self.frames = [snail_1,snail_2]
+            y_pos =300
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900,1100),y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 6
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
@@ -9,40 +83,14 @@ def display_score():
     screen.blit(score_surf,score_rect)
     return current_time
 
-def obstacle_movement(obstacle_list):
-
-    if obstacle_list:
-        for obstacle_rect in obstacle_list:
-            obstacle_rect.x -=6
-            if obstacle_rect.bottom == 300:
-                screen.blit(snail_surf,obstacle_rect)
-            else:
-                screen.blit(fly_surf,obstacle_rect)
-            
-        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]    
-        
-        return obstacle_list
+def collisions_sprite():
+    if pygame.sprite.spritecollide(player.sprite,obstacle_group,False): 
+        obstacle_group.empty()
+        return False
     else:
-        return []
-
-def collisions(player,obstacles):
-    if obstacles:
-        for obstacle_rect in obstacles:
-            if player.colliderect(obstacle_rect):
-                return False
-    return True            
-
-def player_animation():
-    global player_surf, player_index
+        return True
     
-    if player_rect.bottom < 300:
-        player_surf = player_jump
-    else:
-        player_index +=0.1
-        if player_index >= len(player_walk):
-            player_index = 0
-        player_surf = player_walk[int(player_index)]
-
+#Startup
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Dino Run')
@@ -51,38 +99,19 @@ test_font = pygame.font.Font('Dino-Run/font/Pixeltype.ttf', 50)
 game_active = False
 start_time = 0
 score = 0
+bg_Music = pygame.mixer.Sound('Dino-Run/audio/music.wav')
+bg_Music.set_volume(0.1)
+bg_Music.play(loops = -1)
 
+#Groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
+
+#Backround
 sky_surface = pygame.image.load('Dino-Run/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('Dino-Run/graphics/ground.png').convert()
-
-#score_surf = test_font.render('My Game', False, (64, 64, 64))
-#score_rect = score_surf.get_rect(center=(400, 50))
-#score_background_rect = score_rect.inflate(20, 10)
-
-#Obstacles
-snail_frame1 = pygame.image.load('Dino-Run/graphics/snail/snail1.png').convert_alpha()
-snail_frame2 = pygame.image.load('Dino-Run/graphics/snail/snail2.png').convert_alpha()
-snail_frames = [snail_frame1, snail_frame2]
-snail_frame_index = 0
-snail_surf = snail_frames[snail_frame_index]
-
-fly_frame1 = pygame.image.load('Dino-Run/graphics/Fly/Fly1.png').convert_alpha()
-fly_frame2 = pygame.image.load('Dino-Run/graphics/Fly/Fly2.png').convert_alpha()
-fly_frames = [fly_frame1, fly_frame2]
-fly_frame_index = 0
-fly_surf = fly_frames[fly_frame_index]
-
-obstacle_rect_list = []
-
-player_walk1 = pygame.image.load('Dino-Run/graphics/Player/player_walk_1.png').convert_alpha()
-player_walk2 = pygame.image.load('Dino-Run/graphics/Player/player_walk_2.png').convert_alpha()
-player_walk = [player_walk1,player_walk2]
-player_index = 0
-player_jump = pygame.image.load('Dino-Run/graphics/Player/jump.png').convert_alpha()
-
-player_surf = player_walk[player_index]
-player_rect = player_surf.get_rect(midbottom=(80, 300))
-player_gravity = 0
 
 #Intro Screen
 player_stand = pygame.image.load('Dino-Run/graphics/Player/player_stand.png').convert_alpha()
@@ -99,12 +128,6 @@ game_instruct_rect = game_instruct.get_rect(center = (400,320))
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer,1000)
 
-snail_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(snail_timer,500)
-
-fly_timer = pygame.USEREVENT + 3
-pygame.time.set_timer(fly_timer,200)
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -112,68 +135,42 @@ while True:
             exit()
 
         if game_active:    
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                    player_gravity = -20
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                    player_gravity = -20
-            
-            if event.type == obstacle_timer:
-                if randint(0,2):
-                    obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900,950),300)))
-                else:
-                    obstacle_rect_list.append(fly_surf.get_rect(bottomright = (randint(900,950),190)))  
 
-            if event.type == snail_timer:
-                if snail_frame_index == 0:
-                   snail_frame_index = 1
-                else:
-                    snail_frame_index = 0
-                snail_surf = snail_frames[snail_frame_index]       
-            if event.type == fly_timer:
-                if fly_frame_index == 0:
-                   fly_frame_index = 1
-                else:
-                    fly_frame_index = 0
-                fly_surf = fly_frames[fly_frame_index]   
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly','snail','snail'])))   
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player.sprite.jump()
 
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
+                player.sprite.reset_position()
+
                 start_time = int(pygame.time.get_ticks() / 1000)
         
 
 
 
     if game_active:
+        
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,300))
         
         score = display_score()
         
-        
-        # Player
-        player_gravity += 1
-        player_rect.y += player_gravity
-        if player_rect.bottom >= 300:
-            player_rect.bottom = 300
-        player_animation()    
-        screen.blit(player_surf, player_rect)
+        player.draw(screen)
+        player.update()
 
-        #Obstacle Movement
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        obstacle_group.draw(screen)
+        obstacle_group.update()
         
+        #collision
+        game_active = collisions_sprite()
 
-        #collison
-        game_active = collisions(player_rect,obstacle_rect_list)
-        
     else:        
         screen.fill((94,129,162))
         screen.blit(player_stand,player_stand_rect)
-        obstacle_rect_list.clear()
-        player_rect.midbottom = (80,300)
-        player_gravity = 0
 
         score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
         score_message_rect = score_message.get_rect(center = (400,330))
@@ -183,5 +180,6 @@ while True:
             screen.blit(game_instruct, game_instruct_rect)
         else:
             screen.blit(score_message,score_message_rect)
+    
     pygame.display.update()
     clock.tick(60)
